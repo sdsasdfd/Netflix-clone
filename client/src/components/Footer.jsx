1733b2aa8dd30d082904b5621e4e1aa22189090e
Mirza Unas <unasmirza316@gmail.com>
2024-08-18 11:30:37 +0500

    adding github link and vercel cofig

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
           </a>
           . The source code is available on{" "}
           <a
-            href="https://github.com/sdsasdfd"
+            href="https://github.com/sdsasdfd/Netflix-clone"
             target="_blank"
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
